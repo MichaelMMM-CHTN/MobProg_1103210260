842e08caf835adfb50fc9da6e7d6dda7012803e7
MichaelMMM-CHTN <michaelchristopher14062003@gmail.com>
2024-06-22 18:23:43 +0700

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// build.gradle.kts (Project: TugasBesarMobProh)
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
